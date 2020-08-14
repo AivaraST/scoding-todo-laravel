a67bf60dc4192a3698786cdf7971fc7b3d22f551
Aivaras <stasiulis.aivaras@gmail.com>
2020-08-14 16:16:14 +0300

    Added tasks model/migration/controller

--- app/User.php
+++ app/User.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -36,4 +37,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Relationship for user tasks
+     */
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
 }
