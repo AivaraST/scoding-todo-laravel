ccb274b8a7f9b4a103a0d320d5b717902e363295
Aivaras <stasiulis.aivaras@gmail.com>
2020-08-14 16:07:38 +0300

    Modified user table and added user seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        // Add initial admin user
+        $users = [
+            [
+                'name' => 'Aivaras',
+                'email' => 'stasiulis.aivaras@gmail.com',
+                'password' => bcrypt('admin'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+        ];
+
+        // Add random users
+        for($i = 0; $i < 10; $i++) {
+            $users[] = [
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => bcrypt('userpass'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ];
+        }
+
+        DB::table('users')->insert($users);
+    }
+}
