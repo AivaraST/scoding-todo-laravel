361e36647d855e67a64ad28ac8e7907958f310da
Aivaras <stasiulis.aivaras@gmail.com>
2020-08-20 12:52:56 +0300

    Updated JWTMiddleware

--- app/Http/Middleware/JWTMiddleware.php
+++ app/Http/Middleware/JWTMiddleware.php
@@ -21,11 +21,11 @@ class JWTMiddleware
             $user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
             if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-                return response()->json(['error' => 'Token is Invalid']);
+                return response()->json(['error' => 'Token is Invalid'], 401);
             }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                return response()->json(['error' => 'Token has Expired']);
+                return response()->json(['error' => 'Token has Expired'], 401);
             }else{
-                return response()->json(['error' => 'Authorization Token not found']);
+                return response()->json(['error' => 'Authorization Token not found'], 401);
             }
         }
         return $next($request);
